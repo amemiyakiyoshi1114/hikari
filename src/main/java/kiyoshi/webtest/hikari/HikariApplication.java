0cd63089d7c75b9c0cd16fef6760712f41f69d70
水玻璃 <oncwnuCfPfDAbiZYO5gZXMt5oqu4@git.weixin.qq.com>
2024-03-20 21:50:45 +0800

    initial

---
+++ src/main/java/kiyoshi/webtest/hikari/HikariApplication.java
@@ -0,0 +1,13 @@
+package kiyoshi.webtest.hikari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikariApplication.class, args);
+	}
+
+}
